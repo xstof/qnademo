eedde745823c4a512b83b9d95146d43421789f8e
Christof <xstofgit@outlook.com>
2020-02-19 20:18:52 +0100

    add resilience and update fn setting with fn deploy as well

--- deployment/deploy-function-package-global.ps1
+++ deployment/deploy-function-package-global.ps1
@@ -13,7 +13,29 @@ $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $pathForPackage = Join-Path -Path $scriptPath -ChildPath $PackagePath
 
 Foreach ($deploy in $regionDeploys){
+  # publish function
   Publish-AzWebapp -ResourceGroupName $deploy.resourceGroup -Name $deploy.functionAppName -ArchivePath $pathForPackage -Force
+
+  # (section below is optional, also done by deploy-infra.ps1 script) update function settings:
+      $signalRName = $deploy.signalrName
+      $functionName = $deploy.functionAppName
+      $key = Get-AzSignalRKey -ResourceGroupName $deploy.resourceGroup -Name $signalRName
+
+      # Get Function App Settings:
+      $functionApp = Get-AzWebApp -ResourceGroupName $deploy.resourceGroup -Name $functionName
+      $functionAppSettings = $functionApp.SiteConfig.AppSettings
+      $hashTableWithSettings = @{}
+      ForEach ($item in $functionAppSettings){
+        if ($item.Name -eq "AzureSignalRConnectionString") {    
+          $hashTableWithSettings[$item.Name] = $key.PrimaryConnectionString
+        } else {
+          $hashTableWithSettings[$item.Name] = $item.Value
+        }
+      }
+
+      # Set Function App Settings:
+      Set-AzWebApp -ResourceGroupName $deploy.resourceGroup -Name $functionName -AppSettings $hashTableWithSettings
+      Write-Output "changed function $functionName web app settings for SignalR to: $($key.PrimaryConnectionString)"
 }
 
 Write-Output "Function app packages deployed to: "
